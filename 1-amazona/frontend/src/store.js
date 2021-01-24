3969107730f8ec006c4eec9430d67c99f066ffe7
bahtiyar <bahtiyar1989@gmail.com>
2021-01-24 17:29:42 +0300

    order added

--- 1-amazona/frontend/src/store.js
+++ 1-amazona/frontend/src/store.js
@@ -1,6 +1,6 @@
-import { createStore, compose, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import { cartReducer } from "./reducers/cartReducers";
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import {
   orderCreateReducer,
   orderDeleteReducer,
@@ -9,7 +9,7 @@ import {
   orderListReducer,
   orderMineListReducer,
   orderPayReducer,
-} from "./reducers/orderReducers";
+} from './reducers/orderReducers';
 import {
   productCategoryListReducer,
   productCreateReducer,
@@ -18,7 +18,7 @@ import {
   productListReducer,
   productReviewCreateReducer,
   productUpdateReducer,
-} from "./reducers/productReducers";
+} from './reducers/productReducers';
 import {
   userAddressMapReducer,
   userDeleteReducer,
@@ -29,22 +29,22 @@ import {
   userTopSellerListReducer,
   userUpdateProfileReducer,
   userUpdateReducer,
-} from "./reducers/userReducers";
+} from './reducers/userReducers';
 
 const initialState = {
   userSignin: {
-    userInfo: localStorage.getItem("userInfo")
-      ? JSON.parse(localStorage.getItem("userInfo"))
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
       : null,
   },
   cart: {
-    cartItems: localStorage.getItem("cartItems")
-      ? JSON.parse(localStorage.getItem("cartItems"))
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
-    shippingAddress: localStorage.getItem("shippingAddress")
-      ? JSON.parse(localStorage.getItem("shippingAddress"))
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
-    paymentMethod: "PayPal",
+    paymentMethod: 'PayPal',
   },
 };
 const reducer = combineReducers({
@@ -73,7 +73,6 @@ const reducer = combineReducers({
   productReviewCreate: productReviewCreateReducer,
   userAddressMap: userAddressMapReducer,
 });
-//we added redux thunk
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
